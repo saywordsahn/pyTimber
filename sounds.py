e1562051dee34a1f2792c70629afcecb841a5929
TCC - Admin <saywordsahn@gmail.com>
2021-12-11 17:24:07 +0900

    corrected folder for source wavs

--- sounds.py
+++ sounds.py
@@ -7,11 +7,11 @@ class Sounds:
         pygame.mixer.pre_init(44100, -16, 2, 512)
         mixer.init()
 
-        self.chop_fx = pygame.mixer.Sound('img/chop.wav')
+        self.chop_fx = pygame.mixer.Sound('sounds/chop.wav')
         self.chop_fx.set_volume(volume)
 
-        self.death_fx = pygame.mixer.Sound("img/death.wav")
+        self.death_fx = pygame.mixer.Sound("sounds/death.wav")
         self.death_fx.set_volume(volume)
 
-        self.out_of_time_fx = pygame.mixer.Sound("img/out_of_time.wav")
+        self.out_of_time_fx = pygame.mixer.Sound("sounds/out_of_time.wav")
         self.out_of_time_fx.set_volume(volume)
